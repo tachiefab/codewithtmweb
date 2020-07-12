c87f374a921fb0b31a20b2fd5e0e9debc56ff8c4
unknown <tachiefab311@gmail.com>
2020-07-12 14:36:25 -0700

    Created core, block and shared modules and added static files in the assets folder

---
+++ src/app/block/block.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlockRoutingModule } from './block-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BlockRoutingModule
+  ]
+})
+export class BlockModule { }
