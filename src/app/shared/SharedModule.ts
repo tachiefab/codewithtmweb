2e04d79531ee9cb46dd7fd2c0453040605b5b637
unknown <tachiefab311@gmail.com>
2020-09-02 15:55:48 -0700

    initial commit

--- src/app/shared/SharedModule.ts
+++ src/app/shared/SharedModule.ts
@@ -1,29 +1,26 @@
+
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
 import { SharedRoutingModule } from './shared-routing.module';
 import { PaginationComponent } from './pagination/pagination.component';
 import { SafePipe } from './utility/safe.pipe';
 import { LogoComponent } from './logo/logo.component';
 // import { AuthUserService } from './utility/authUser.service';
-
 @NgModule({
-  declarations: [
-    PaginationComponent,
-    SafePipe,
-    LogoComponent,
-    // AuthUserService
-  ],
-  imports: [
-    CommonModule,
-    NgModule,
-    SharedRoutingModule
-  ],
- exports: [
-    SafePipe,
-    LogoComponent,
-    // AuthUserService,
-  ]
+    declarations: [
+        PaginationComponent,
+        SafePipe,
+        LogoComponent,
+    ],
+    imports: [
+        CommonModule,
+        // NgModule,
+        SharedRoutingModule
+    ],
+    exports: [
+        SafePipe,
+        LogoComponent,
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
