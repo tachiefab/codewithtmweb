7e51518567b3497a8092d9f7142c2c19e953f2e7
unknown <tachiefab311@gmail.com>
2021-01-21 21:02:31 -0800

    initial

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { SearchDetailComponent } from './search-detail/search-detail.component';
 import { SearchRoutingModule } from './search-routing.module';
 import { SharedModule } from '../shared/SharedModule';
-import { PostCardComponent } from '../blog/post-card/post-card.component';
+// import { PostCardComponent } from '../blog/post-card/post-card.component';
 // import { PostCardComponent } from './post-card/post-card.component';
 
 
