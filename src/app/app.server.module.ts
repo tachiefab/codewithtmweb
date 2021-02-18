a639c7c7dd07f9542fdd52b0b45c36a9ca37e726
tachiefab <tachiefab311@gmail.com>
2021-02-18 09:10:50 -0800

    errors in interceptor

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { NgModule } from '@angular/core';
 // import { ServerModule } from '@angular/platform-server';
 
@@ -47,53 +46,3 @@ import { TokenInterceptor } from './auth/token.interceptor';
   ]
 })
 export class AppServerModule {}
-=======
-import { NgModule } from '@angular/core';
-import { ServerModule } from '@angular/platform-server';
-
-import { AppModule } from './app.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  imports: [
-    AppModule,
-    ServerModule,
-  ],
-  bootstrap: [AppComponent],
-})
-export class AppServerModule {}
-
-
-// import { NgModule } from '@angular/core';
-// import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
-
-// import { AppModule } from './app.module';
-// import { AppComponent } from './app.component';
-// import { BrowserModule, TransferState } from '@angular/platform-browser';
-// import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-// import { HTTP_INTERCEPTORS } from '@angular/common/http';
-// import { TokenInterceptor } from './auth/token.interceptor';
-// // import { TokenInterceptor, DEFAULT_TIMEOUT } from './auth/token.interceptor';
-
-// @NgModule({
-//   imports: [
-//     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-//     AppModule,
-//     ServerModule,
-//     NoopAnimationsModule,
-//     ServerTransferStateModule
-//   ],
-//   bootstrap: [AppComponent],
-//   providers: [
-//     TransferState
-//     // {
-//     //   provide: HTTP_INTERCEPTORS,
-//     //   useClass: TokenInterceptor,
-//     //   multi: true
-//     // },
-//     // { provide: DEFAULT_TIMEOUT, useValue: 30000 },
-//     TransferState
-//   ]
-// })
-// export class AppServerModule {}
->>>>>>> 055ef3bbe72d20b9f759794151fa22157a398c8a
